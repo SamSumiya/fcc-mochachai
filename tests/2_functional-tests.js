766577079c9e76450be9097697ade2945211d661
Sam Sumiya <60025491+SamSumiya@users.noreply.github.com>
2025-05-16 16:23:52 -0700

    fixed

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -52,16 +52,18 @@ suite('Functional Tests', function () {
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
       chai
-        .request(server)
-        .put('/travellers')
-        .send({surname: "da Verrazzano"})
-        .end(function(err, res) {
-          assert.equal(res.status, 200);
-          assert.equal(res.type, 'application/json');
-          assert.equal(res.body.name, 'Giovanni');
-          assert.equal(res.body.surname, 'da Verrazzano');
-          done();
-        })
+      .request(server)
+      .put('/travellers')
+      .send({ surname: 'da Verrazzano' })
+      /** place your tests inside the callback **/
+      .end(function(err, res) {
+        assert.equal(res.status, 200, 'response status should be 200');
+        assert.equal(res.type, 'application/json', 'Response should be json');
+        assert.equal(res.body.name, 'Giovanni');
+        assert.equal(res.body.surname, 'da Verrazzano');
+
+        done();
+      });
     });
   });
 });
