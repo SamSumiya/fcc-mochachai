8d7de9b4411761242567e1960c5c271f0ad3b845
Sam Sumiya <60025491+SamSumiya@users.noreply.github.com>
2025-05-16 16:14:26 -0700

    fixed to strictEqual

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -57,10 +57,10 @@ suite('Functional Tests', function () {
         .put('/travellers')
         .send({surname: "da Verrazzano"})
         .end(function(err, res) {
-          assert.equal(res.status, 200);
-          assert.equal(res.type, 'application/json');
-          assert.equal(res.body.surname, 'da Verrazzano');
-          assert.equal(res.body.name, 'Giovanni');
+          assert.strictEqual(res.status, 200);
+          assert.strictEqual(res.type, 'application/json');
+          assert.strictEqual(res.body.surname, 'da Verrazzano');
+          assert.strictEqual(res.body.name, 'Giovanni');
           done();
         })
     });
