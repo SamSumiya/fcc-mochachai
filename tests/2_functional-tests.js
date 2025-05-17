3f4ade8fbbb36ba884693414baa0d5c4c4936186
Sam Sumiya <60025491+SamSumiya@users.noreply.github.com>
2025-05-16 23:54:12 -0700

    fixing

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -125,21 +125,14 @@ suite('Functional Tests with Zombie.js', function () {
       // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
       browser.visit('/', function () {
-        browser.wait().then(() => {
-          browser
-            .fill('surname', 'Vespucci')
-            .then(() => {
-              browser.pressButton('submit', function () {
+        browser.fill('surname', 'Vespucci').pressButton('submit', function () {
                 browser.assert.success();
                 browser.assert.text('span#name', 'Amerigo');
                 browser.assert.text('span#surname', 'Vespucci');
                 browser.assert.element('span#dates', 1);
-                    done();
+                done();
               });
             });
         });
-     
-      });
     });
-  });
 });
